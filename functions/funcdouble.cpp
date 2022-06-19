a2a389f46840dc421491bec95c2a401a47e62138
Latishfaction <latishchambhare@gmail.com>
2022-06-19 19:27:27 +0530

    num double w/ fuinction parameter

---
+++ functions/funcdouble.cpp
@@ -0,0 +1,19 @@
+/*
+
+Write a function called doubleNumber() that takes one integer parameter. The function should return double the value of the parameter.
+
+https://www.learncpp.com/cpp-tutorial/introduction-to-function-parameters-and-arguments/
+*/
+
+#include <iostream>
+
+int doubleNumber(int num){
+    return num*2;
+}
+
+int main(){
+    std::cout<<"Enter the number to be doubled : ";
+    int x{};
+    std::cin>>x;
+    std::cout<<doubleNumber(x);
+}
