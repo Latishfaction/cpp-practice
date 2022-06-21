21a9b238b7dc6bcd2bc70a76705be67629c6b425
Latishfaction <latishchambhare@gmail.com>
2022-06-21 20:41:31 +0530

    soved quiz 1

---
+++ 2x/q1.cpp
@@ -0,0 +1,39 @@
+/*
+Write a single-file program (named main.cpp) that reads two separate integers from the user, adds them together, and then outputs the answer. The program should use three functions:
+
+    > A function named “readNumber” should be used to get (and return) a single integer from the user.
+    > A function named “writeAnswer” should be used to output the answer. This function should take a single parameter and have no return value.
+    > A main() function should be used to glue the above functions together.
+
+    * Hint: You do not need to write a separate function to do the adding (just use operator+ directly).
+
+    * Hint: You will need to call readNumber() twice.
+*/
+
+#include <iostream>
+
+int readNumber();
+int writeAnswer(int x,int y);
+
+int main(){
+    int x{readNumber()};
+    int y{readNumber()};
+
+    // get the number
+    std::cout<<"Addition of "<<x<<" and "<<y<<" is "<<writeAnswer(x,y);
+
+}
+
+int readNumber(){
+    int x{};
+    std::cout<<"Enter the value : ";
+    std::cin>>x;
+
+    return x;    
+}
+
+int writeAnswer(int x,int y){
+    int sum{};
+    sum = x+y;
+    return sum;
+}
