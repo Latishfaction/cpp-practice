a00c99e7e467e9fbbda91e3f4256071fbef7cc47
Latishfaction <latishchambhare@gmail.com>
2022-06-24 14:48:09 +0530

    bits of data type

---
+++ 4x/data type/sizeint.cpp
@@ -0,0 +1,14 @@
+/*
+short = 16 bits (2^4)
+int = 512 bits (2^9)
+long = 512 bits (2^9)
+long long = 2,62,144 bit (2^18)
+*/
+
+#include <iostream>
+
+int main(){
+    long long int x = 999999999999999999;
+    std::cout<<x;
+    return 0;
+}
