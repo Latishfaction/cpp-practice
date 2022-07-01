a6776e644077975001031bc65ac8c9898495590b
Latishfaction <latishchambhare@gmail.com>
2022-07-01 15:25:50 +0530

    quiz 1

---
+++ 7x/quizzes/q1.cpp
@@ -0,0 +1,51 @@
+/*
+
+https://www.learncpp.com/cpp-tutorial/chapter-7-comprehensive-quiz/
+
+Question #1
+
+In the chapter 4 comprehensive quiz, we wrote a program to simulate a ball falling off of a tower. Because we didn’t have loops yet, the ball could only fall for 5 seconds.
+
+Take the program below and modify it so that the ball falls for as many seconds as needed until it reaches the ground.
+
+*/
+
+#include <iostream>
+#include "constants.h"
+
+double calculateHeight(double initialHeight, int seconds)
+{
+    double distanceFallen { myConstants::gravity * seconds * seconds / 2 };
+    double heightNow { initialHeight - distanceFallen };
+
+    // Check whether we've gone under the ground
+    // If so, set the height to ground-level
+    if (heightNow < 0.0)
+        return 0.0;
+    else
+        return heightNow;
+}
+
+// Returns true if the ball hit the ground, false if the ball is still falling
+bool calculateAndPrintHeight(double initialHeight, int time)
+{
+    double currentHeight { calculateHeight(initialHeight, time) };
+    std::cout << "At " << time << " seconds, the ball is at height: " << currentHeight << "\n";
+
+    return (currentHeight == 0.0);
+}
+
+int main()
+{
+    std::cout << "Enter the initial height of the tower in meters: ";
+    double initialHeight;
+    std::cin >> initialHeight;
+
+    int seconds = 0;
+
+    // returns true if the ground was hit
+    while (!calculateAndPrintHeight(initialHeight, seconds))
+        ++seconds;
+
+    return 0;
+}
