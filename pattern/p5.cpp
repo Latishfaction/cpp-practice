2b19997c7c84a6f6dce79dbce9cf230b6e6d3a06
Latishfaction <latishchambhare@gmail.com>
2022-07-05 14:07:23 +0530

    number pattern

---
+++ pattern/p5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+
+    //rows
+    for(int i=0;i<n;i++){
+        //columns
+        for(int j=0;j<=i;j++){
+            // printing no. of the current row
+            cout<<i+1<<" ";
+        }
+        cout<<"\n";
+    }
+}
